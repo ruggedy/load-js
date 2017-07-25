00b95831fa2ddaea802e0e12d2193e6d5a89c166
Miguel Castillo <mcastillo@symphono.com>
2017-07-25 15:34:46 -0400

    added logic to detect if a script is being loaded and it already exists in the DOM

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
   }
 })(this, function() {
   var cache = {};
+  var head = document.getElementsByTagName("head")[0] || document.documentElement;
 
   function exec(options) {
     if (typeof options === "string") {
@@ -20,36 +21,27 @@
     }
 
     var id = options.id || options.url;
-    var script = cache[id];
+    var cacheEntry = cache[id];
 
-    if (script) {
-      console.log('load-js cache hit', id);
-      return script;
+    if (cacheEntry) {
+      console.log("load-js: cache hit", id);
+      return cacheEntry;
+    }
+    else {
+      var el = getScriptById(options.id) || getScriptByUrl(options.url);
+      if (el && id && options.cache !== false) {
+        cache[id] = Promise.resolve(el);
+        return cache[id];
+      }
     }
 
     if (!options.url && !options.text) {
-      throw new Error("must provide a url or text to load");
+      throw new Error("load-js: must provide a url or text to load");
     }
 
-    script = document.createElement("script");
-    script.charset = options.charset || "utf-8";
-    script.type = options.type || "text/javascript";
-    script.async = !!options.async;
-    script.id = id;
+    var pending = (options.url ? loadScript : runScript)(head, createScript(options));
 
-    var head = document.getElementsByTagName("head")[0] || document.documentElement;
-    var pending;
-
-    if (options.url) {
-      script.src = options.url;
-      pending = loadScript(head, script);
-    }
-    else {
-      script.text = options.text;
-      pending = runScript(head, script);
-    }
-
-    if (options.cache !== false && id) {
+    if (id && options.cache !== false) {
       cache[id] = pending;
     }
 
@@ -87,6 +79,43 @@
 
       head.appendChild(script);
     });
+  }
+
+  function createScript(options) {
+    var script = document.createElement("script");
+    script.charset = options.charset || "utf-8";
+    script.type = options.type || "text/javascript";
+    script.async = !!options.async;
+    script.id = options.id || options.url;
+    script.loadJS = "watermark";
+
+    if (options.url) {
+      script.src = options.url;
+    }
+
+    if (options.text) {
+      script.text = options.text;
+    }
+
+    return script;
+  }
+
+  function getScriptById(id) {
+    var script = id && document.getElementById(id);
+
+    if (script && script.loadJS !== "watermark") {
+      console.warn("load-js: duplicate script with id:", id);
+      return script;
+    }
+  }
+
+  function getScriptByUrl(url) {
+    var script = url && document.querySelector("script[src='" + url + "']");
+
+    if (script && script.loadJS !== "watermark") {
+      console.warn("load-js: duplicate script with url:", url);
+      return script;
+    }
   }
 
   return function load(items) {
